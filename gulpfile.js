ffb5076f051028a5a1b765e1ba8651a0d5155532
Joe Toscano <realjoet9@gmail.com>
2016-01-02 17:38:45 -0700

    styles set for email submission, cards not added yet

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,31 @@ var gulp = require('gulp'),
   livereload = require('gulp-livereload'),
   sass = require('gulp-ruby-sass');
 
+// Changed to pipe Sass from app directory
 gulp.task('sass', function () {
-  console.log('piping');
   return sass('./app/styles/*.scss')
     .pipe(gulp.dest('./public/css'))
     .pipe(livereload());
 });
 
+// NEED TO PIPE JS AND ASSETS TO ./PUBLIC
+
+// gulp.task('js', function () {
+//   return js('./app/js/*.js')
+//     .pipe(gulp.dest('./public/js'))
+//     .pipe(livereload());
+// })
+
+// gulp.task('.img', function () {
+//   return img('.app/assets/**/*.jpg')
+//     .pipe(gulp.dest('./public/img'))
+//     .pipe(livereload);
+// })
+
 gulp.task('watch', function() {
-  console.log('watching');
   gulp.watch('./app/styles/**/*.scss', ['sass']);
+  // gulp.watch('./app/js/*.js', ['js']);
+  // gulp.watch('./app/assets/**/*.jpg', ['img']);
 });
 
 gulp.task('develop', function () {
@@ -35,6 +50,7 @@ gulp.task('develop', function () {
 
 gulp.task('default', [
   'sass',
+  // 'js',
   'develop',
   'watch'
 ]);
