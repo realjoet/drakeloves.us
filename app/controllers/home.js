582d7d3ff17db266a85536761508f8b454ffd4a4
Joe Toscano <realjoet9@gmail.com>
2016-01-31 20:45:29 -0700

    messing with FB sharing optimization and new routes for hosting images from site

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -16,3 +16,65 @@ router.get('/', function (req, res, next) {
     });
   });
 });
+
+// Test to see if works
+// re-route back to home page after timeout
+router.get('/whats-my-name', function (req, res, next) {
+  Article.find(function (err, articles) {
+    if (err) return next(err);
+    res.render('whats-my-name', {
+      title: "Drake's Valentine Club",
+      articles: articles
+    });
+  });
+});
+
+router.get('/in-the-morning-2', function (req, res, next) {
+  Article.find(function (err, articles) {
+    if (err) return next(err);
+    res.render('index', {
+      title: "Drake's Valentine Club",
+      articles: articles
+    });
+  });
+});
+
+router.get('/0-100', function (req, res, next) {
+  Article.find(function (err, articles) {
+    if (err) return next(err);
+    res.render('index', {
+      title: "Drake's Valentine Club",
+      articles: articles
+    });
+  });
+});
+
+router.get('/best-i-ever-had', function (req, res, next) {
+  Article.find(function (err, articles) {
+    if (err) return next(err);
+    res.render('index', {
+      title: "Drake's Valentine Club",
+      articles: articles
+    });
+  });
+});
+
+router.get('/hotline-bling', function (req, res, next) {
+  Article.find(function (err, articles) {
+    if (err) return next(err);
+    res.render('index', {
+      title: "Drake's Valentine Club",
+      articles: articles
+    });
+  });
+});
+
+router.get('/in-the-morning', function (req, res, next) {
+  Article.find(function (err, articles) {
+    if (err) return next(err);
+    res.render('index', {
+      title: "Drake's Valentine Club",
+      articles: articles
+    });
+  });
+});
